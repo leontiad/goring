83990cc766ce1906c0d48567d99d32f81c359f81
Iraklis Leontiadis <leontiad@gmail.com>
2025-05-20 17:41:11 +0300

    fix netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				target: process.env.VITE_API_URL || 'http://localhost:3001',
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	}
 });
